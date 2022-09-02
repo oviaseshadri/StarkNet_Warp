d8ebadd8c747f51c268d34b3d17fd1f8bcf166ec
gyan0890 <gyanlakshmi@gmail.com>
2022-09-03 04:14:32 +0530

    Code Warped

--- DCM.sol
+++ DCM.sol
@@ -1,13 +1,69 @@
-
 // SPDX-License-Identifier: GPL-3.0
 
 pragma solidity >=0.7.0 <0.9.0;
-import "@openzeppelin/contracts/utils/Counters.sol";
+
+interface IERC20 {
+  function mint(address to, uint256 amount) external returns (bool);
+
+  function balanceOf(address account) external view returns (uint256);
+
+  function decimals() external pure returns (uint8);
+
+  function transferFrom(
+    address sender,
+    address recipient,
+    uint256 amount
+  ) external returns (bool);
+}
+
+contract DCMEscrow {
+
+    uint _escrowIds = 0;
+    address eth_contract_address;
+    
+    struct Escrow {
+        uint escrowId;
+        uint amount;
+        uint projectId;
+        uint projectStartDate;
+        uint projectEndDate;
+        address payer;
+        address payee;
+    }
+
+    event EscrowCreated(uint eId, uint pId);
+
+    mapping(uint => Escrow) idToEscrowMapping;
+    
+    constructor(address contract_addr) {
+        eth_contract_address = contract_addr;
+    }
+
+    function createEscrow(uint _amount, uint _projectId, uint _projectStartDate, uint _projectEndDate, address _payee, address _payer) public returns(uint) {
+        _escrowIds += 1;
+        idToEscrowMapping[_escrowIds] = Escrow(_escrowIds, _amount, _projectId, _projectStartDate, _projectEndDate, _payer, _payee);
+
+        return _escrowIds;
+        
+    }
+
+    function escrowSettle(uint eId) public {
+        Escrow storage e = idToEscrowMapping[eId];
+        IERC20(eth_contract_address).transferFrom(e.payer, e.payee, e.amount);
+    }
+
+
+    //******** GET FUNCTIONS *******************//
+
+    function getEscrow(uint eId) public view returns(Escrow memory) {
+        return idToEscrowMapping[eId];
+    }
+}
 
 contract DCM {
 
-    using Counters for Counters.Counter;
-    Counters.Counter private _projectIds;
+    uint _projectIds = 0;
+    address escrow_contract_address;
 
     enum ProjectStatus {
         INITIATED,
@@ -50,11 +106,12 @@ contract DCM {
     event EscrowCreated(address indexed escrowAddress, uint indexed amount, uint indexed projectId);
     event EscrowSettled(uint projectId);
 
-    //DAO -> Project ID Mapping
-    mapping(address => uint[]) daoToProjectsMap;
 
     //Project -> Escrow Contract
     mapping(uint => address) projectToEscrowMap;
+
+    // Project ID -> Escrow ID
+    mapping(uint => uint) projectIdToEscrowId;
 
     //Project ID -> Project Struct
     mapping(uint => Project) projectIdToProjectMap;
@@ -62,30 +119,23 @@ contract DCM {
     //User ID -> User Struct
     mapping(address => Contributor) contributorIdToUserMap;
 
-    modifier onlyDao(address _dao, uint _id) {
-        uint isDao = false;
-        for(uint i = 0; i < daoToProjectsMap[_dao]; i++){
-            if(_id == daoToProjectsMap[_dao][i]) {
-                isDao = true;
-            }
-        }
-
-        require(isDao, "The project doesn't belong to this DAO");
-        _;
+    constructor(address contract_addr) {
+        escrow_contract_address = contract_addr;
     }
+
     //***************** Write functions *****************//
     
     //Add a new contributor
     function createContributor(string memory _name, string memory _description, string memory _linkedIn, string memory _github, uint _averageRatePerHour) public returns(address) {
-        userIdToUserMap[msg.sender] = Contributor(msg.sender, _name, _description, _linkedIn, _github, _averageRatePerHour, 0);
-        contributors.push(userIdToUserMap[msg.sender]);
+        contributorIdToUserMap[msg.sender] = Contributor(msg.sender, _name, _description, _linkedIn, _github, _averageRatePerHour, 0);
+        contributors.push(contributorIdToUserMap[msg.sender]);
 
         return msg.sender;
     }
 
     function createProject(string memory _name, string memory _description, uint _maxCompensation, uint _startDate, uint _endDate, address _dao, address _contributor) internal returns(uint) {
-        _projectIds.increment();
-        uint projectId = _projectIds.current();
+        _projectIds = _projectIds + 1;
+        uint projectId = _projectIds;
 
         projectIdToProjectMap[projectId] = Project(projectId, _name, _description, _maxCompensation, _startDate, _endDate, _dao, _contributor, ProjectStatus.INREVIEW);
 
@@ -94,87 +144,37 @@ contract DCM {
         return projectId;
     }
 
-    //Current logic states that the one who is invited is auomatically going to work on the project, not for productio/// @notice Explain to an end user what this does
-    /// @dev Explain to a developer any extra details
-    /// @param Documents a parameter just like in doxygen (must be followed by parameter name)
-    // MaxConmpensation = x*10^18
+    //Current logic states that the one who is invited is auomatically going to work on the project, not for production
+    //MaxCompensation = x*10^18
     function inviteContributor(address contributor, string memory _name, string memory _description, uint _maxCompensation, uint _startDate, uint _endDate) public payable {
         //1. Create a Project
         uint projectId = createProject(_name, _description, _maxCompensation, _startDate, _endDate, msg.sender, contributor);
-        daoToProjectsMap[msg.sender].push(projectId);
 
         //2. Spin up an escrow with maxCompensation
-        Escrow escrowContract = new Escrow(_maxCompensation, projectId, _startDate, _endDate, contributor, msg.sender);
-        projectToEscrowMap[projectId] = address(escrowContract);
-
-        //3. Transfer money into Escrow contract once it is created.
-        address(escrowContract).transfer(_maxCompensation);
-
-        emit EscrowCreated(address(escrowContract), _maxCompensation, projectId);
-    }
-
-    function approveProject(uint projectId) external onlyDao(msg.sender, projectId) {
-        Escrow escrowContract = Escrow(projectToEscrowMap[projectId]);
-        escrowContract.settle();
-
-        Project storage project = projectIdToProjectMap[projectId];
-        project.status = ProjectStatus.COMPLETED;
-
-        emit EscrowSettled(projectId);
+        projectToEscrowMap[projectId] = escrow_contract_address;
+        uint eId = DCMEscrow(escrow_contract_address).createEscrow(_maxCompensation, projectId, _startDate, _endDate, contributor, msg.sender);
+        projectIdToEscrowId[projectId] = eId;
 
     }
 
+    function approveProject(uint projectId) external {
+        //DCMEscrow escrowContract = DCMEscrow(projectToEscrowMap[projectId]);
+        uint eId = projectIdToEscrowId[projectId];             
+        DCMEscrow(escrow_contract_address).escrowSettle(eId);
+
+        //Project storage project = projectIdToProjectMap[projectId];
+        //project.status = ProjectStatus.COMPLETED;
+
+    }
     //******************* Read functions *****************//
 
-    function getAllProjects() public returns(Project[] memory) {
-        return projects;
-    }
-
-    function getAllContributors() public returns(Contributor[] memory) {
-        return contributors;
+    //Get contributor details from their address
+    function getAllContributors(address _contributor) public view returns(string memory, string memory, uint) {
+        return (contributorIdToUserMap[_contributor].name, contributorIdToUserMap[_contributor].description, contributorIdToUserMap[_contributor].avgRatePerHour);
     } 
 
-    function getProjectFromId(uint projectId) public returns(Project memory) {
-        return projectIdToProjectMap[projectId];
-    }
-
-}
-
-contract DCMEscrow {
-    uint amount;
-    uint projectId;
-    uint projectStartDate;
-    uint projectEndDate;
-    address payer;
-    address payee;
-
-    enum Status {
-        CREATED,
-        SETTLED
-    }
-
-    modifier onlyPayee() {
-        require(msg.sender == payee);
-    }
-
-    constructor(uint _amount, uint _projectId, uint _projectStartDate, uint _projectEndDate, address _payee, address _payer) payable {
-     amount = _amount;
-     projectId = _projectId;
-     projectStartDate = _projectStartDate;
-     projectEndDate = _projectEndDate;
-     payer = _payer;
-     payee = _payee;
-
-    }
-
-    function settle() public {
-        requires(balance(this) == amount, "The Escrow contract does not have any balance");
-        payable(payee).transfer(balance(this));
-    }
-
-    function cancelEscrow() onlyPayee {
-        requires(balance(this) == amount, "The Escrow contract does not have any balance");
-        payable(payer).transfer(balance(this));
+    function getProjectFromId(uint projectId) public view returns(uint, uint,  address, address) {
+        return (projectIdToProjectMap[projectId].id, projectIdToProjectMap[projectId].maxCompensation, projectIdToProjectMap[projectId].dao, projectIdToProjectMap[projectId].user);
     }
 
 }
